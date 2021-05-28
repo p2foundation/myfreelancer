5735998e0e032d4b3a35be42697262c43f0515f5
p2foundation <p2founda@gmail.com>
2021-05-28 16:06:14 +0000

    init myfreelancer marketplace platform

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,2 @@
+# Stripe.api_key = Rails.application.credentials[Rails.env.to_sym][:stripe][:STRIPE_SECRET_KEY]
+Stripe.api_key = "sk_test_51IfTicAR22k9xNh2Tjd3uVQUWvSQIeGWVnDLOyZP4kLa4Agyk4AneMhzSVhixRB6xFmkIrl3Jpmy7Ii33agJ4NvF00jgta54Cf"
---
+++ config/routes.rb
@@ -0,0 +1,45 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+
+  get '/dashboard', to: 'users#dashboard'
+  get '/users/:id', to: 'users#show', as: 'users'
+  get '/selling_orders', to: 'orders#selling_orders'
+  get '/buying_orders', to: 'orders#buying_orders'
+  get '/all_requests', to: 'requests#list'
+  get '/request_offers/:id', to: 'requests#offers', as: 'request_offers'
+  get '/my_offers', to: 'requests#my_offers'
+  get '/search', to: 'pages#search'
+  get '/settings/payment', to: 'users#payment', as: 'settings_payment'
+  get '/settings/payout', to: 'users#payout', as: 'settings_payout'
+  get '/gigs/:id/checkout/:pricing_type', to: 'gigs#checkout', as: 'checkout'
+  get '/earnings', to: 'users#earnings', as: 'earnings'
+
+  post '/users/edit', to: 'users#update'
+  post '/offers', to: 'offers#create'
+  post '/reviews', to: 'reviews#create'
+  post '/users/edit_phone', to: 'users#callback_phone'
+  post '/settings/payment', to: 'users#update_payment', as: "update_payment"
+  post '/settings/payout', to: 'users#update_payout', as: "update_payout"
+  post '/users/withdraw', to: 'users#withdraw', as: 'withdraw'
+
+  put '/orders/:id/complete', to: 'orders#complete', as: 'complete_order'
+  put '/offers/:id/accept', to: 'offers#accept', as: 'accept_offer'
+  put '/offers/:id/reject', to: 'offers#reject', as: 'reject_offer'
+
+  resources :gigs do
+    member do
+      delete :delete_photo
+      post :upload_photo
+    end
+    resources :orders, only: [:create]
+  end
+
+  resources :requests
+
+  devise_for :users, 
+              path: '', 
+              path_names: {sign_up: 'register', sign_in: 'login', edit: 'profile', sign_out: 'logout'},
+              controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
+
+end
